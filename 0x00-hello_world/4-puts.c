465949eaab138151a639cd4e3d414c7aad105883
PRUDY1026 <prudencemaserame@gmail.com>
2023-09-07 16:22:04 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- *  *main - prints a string with puts()
- *   *
- *    *Return: NULL
- *     */
+ * main - Entry point
+ *
+ * Hello C
+ *
+ * Return: Always 0(Success)
+ */
 int main(void)
 {
-	         puts("\"Programming is like building a multilingual puzzle");
-		         return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
+
