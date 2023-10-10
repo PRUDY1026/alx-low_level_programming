8761d83139e9ce2f78396f3931b2ea969c0eae1a
PRUDY1026 <prudencemaserame@gmail.com>
2023-10-09 19:30:34 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+
+#define DOG_H
+
+
+/**
+ * struct dog - a struct that create a dog infor
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Description: first struct of alx
+ */
+
+struct dog
+
+{
+
+	char *name;
+
+	float age;
+
+	char *owner;
+
+};
+
+
+#endif
