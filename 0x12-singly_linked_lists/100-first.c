677c57f326e1e98754a6a1b977a70a4cb6377a15
PRUDY1026 <prudencemaserame@gmail.com>
2023-10-19 17:08:49 -0700

    task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -13,8 +13,8 @@ void first(void)
 
 {
 
- printf("You're beat! and yet, you must allow,\n");
+printf("You're beat! and yet, you must allow,\n");
 
- printf("I bore my house upon my back!\n");
+printf("I bore my house upon my back!\n");
 
 }
